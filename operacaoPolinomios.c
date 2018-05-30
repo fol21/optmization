f06ea3ea70bf1c4ef5c65bffd812f92083f970d6
Fernando de Oliveira Lima <fernando.lima@radixeng.com.br>
2018-05-30 11:41:42 -0300

    - Source files

---
+++ operacaoPolinomios.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "include/otimizacaoPolinomioFernandoLima.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	printf("a = %d \n",a);
+	return 0;
+}
